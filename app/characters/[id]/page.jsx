f8186ef2238c5892d287193c925efaf302405058
Charlottepeddle12 <charlotte.peddle12@ed.cna.nl.ca>
2026-03-12 10:07:05 -0230

    Created new branch, worked on statically rendering the APi page

--- app/characters/[id]/page.jsx
+++ app/characters/[id]/page.jsx
@@ -1,5 +1,17 @@
 import Character from "@/components/Character";
 
+export async function generateStaticParams(){
+    const options = {
+                method: "GET",
+                headers: {"Content-Type": "application/json"}
+            }
+   const response = await fetch("https://rickandmortyapi.com/api/character", options)
+   const characters = await response.json();
+   return characters.results.map((character)=>({
+    id:character.id.toString() 
+   }))
+}
+
 export default async function CharacterDetail(props) {
     const {id} = await props.params;
 
