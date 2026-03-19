372e8ec236044a29e76ee213e2d146b7e1985a24
ZCZDinn <christian.dinn12@ed.cna.nl.ca>
2026-03-19 15:08:48 -0230

    Initial Setup, Homepage layout created, Footer component added, placeholders used for now (before sanity configuration). All 4 group members involved, one machine used.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 export default function Header() {
     return (
         <>
+        {/* Header component with logo and navigation links, temporarily the image is out of position and is a placeholder. Styling will be added later */}
+        <Image alt="Bicycle NL Logo" src="/next.svg" width={100} height={100}></Image>
         <Link href="/">Home</Link>
         <Link href="/characters">Characters</Link>
         </>
